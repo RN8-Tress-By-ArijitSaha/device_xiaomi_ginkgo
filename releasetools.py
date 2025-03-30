ba39385d5fef8e9276372115255abaa90bfe1931
wHo-EM-i <ehteshammalik18998@gmail.com>
2025-03-30 12:55:14 +0000

    ginkgo: Add checks for Dynamic Partitions
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -36,6 +36,7 @@ def AddImage(info, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+  info.script.AppendExtra('assert(getprop("ro.boot.super_partition") == "system" || abort("ERROR: This recovery does not support retrofit dynamic partitions."););')
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
