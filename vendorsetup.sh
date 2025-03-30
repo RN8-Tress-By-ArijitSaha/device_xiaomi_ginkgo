2ef0ed52d9ed27057011d2dda6769fd2d0289611
Arijit78 <sahaarijit2007@gmail.com>
2025-03-30 12:50:55 +0000

    ginkgo: Init vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Clone vendor tree
+git clone --depth=1 https://github.com/RN8-Tress-By-ArijitSaha/vendor_xiaomi_ginkgo.git -b vic vendor/xiaomi/ginkgo
+
+# Clone kernel tree
+git clone --depth=1 https://github.com/RN8-Tress-By-ArijitSaha/android_kernel_xiaomi_ginkgo-ryzen.git -b fourteen/dynamic kernel/xiaomi/ginkgo
