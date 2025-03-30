33d3f1650c4d0b5811cb19fe38436241987d588a
Kelvin Zhang <zhangkelvin@google.com>
2025-03-30 13:33:24 +0000

    ginkgo: Allow to access bootable/deprecated-ota targets
    - Ginkgo is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to violet namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- device.mk
+++ device.mk
@@ -455,6 +455,7 @@ PRODUCT_PACKAGES += \
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
